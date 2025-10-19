e0f34bae6cf2c0e562da4e94184b5ca1c5b39647
Q-Idle User <user@qidle.dev>
2025-10-19 13:51:14 +0530

    stage

---
+++ MyContract.sol
@@ -0,0 +1,8 @@
+// Welcome to Q Remix IDE! 
+// Visit all Quranium websites at: https://quranium.org
+//
+// Write your Solidity contract here...
+pragma solidity ^0.8.7;
+contract MyContract {
+    // Your contract code goes here
+}
